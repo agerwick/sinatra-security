402985b4591c55bae1f22517a781eeb912b9ff07
Cyril David <cyx.ucron@gmail.com>
2010-10-13 15:51:29 +0800

    Space gardening, use start_with? instead of a regex for class-level require_login.

--- lib/sinatra/security.rb
+++ lib/sinatra/security.rb
@@ -1,26 +1,26 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 module Sinatra
   module Security
     VERSION = "0.2.1"
 
-    autoload :Helpers,        'sinatra/security/helpers'
-    autoload :User,           'sinatra/security/user'
-    autoload :Validations,    'sinatra/security/validations'
-    autoload :Password,       'sinatra/security/password'
-    autoload :Identification, 'sinatra/security/identification'
-    autoload :LoginField,     'sinatra/security/login_field'
+    autoload :Helpers,        "sinatra/security/helpers"
+    autoload :User,           "sinatra/security/user"
+    autoload :Validations,    "sinatra/security/validations"
+    autoload :Password,       "sinatra/security/password"
+    autoload :Identification, "sinatra/security/identification"
+    autoload :LoginField,     "sinatra/security/login_field"
 
-    def self.registered(app)
-      app.helpers Helpers
+    def self.registered(m)
+      m.helpers Helpers
 
-      app.set :login_success_message, "You have successfully logged in."
-      app.set :login_error_message, "Wrong Email and/or Password combination."
-      app.set :login_url,           "/login"
-      app.set :login_user_class,    lambda { ::User }
-      app.set :ignored_by_return_to, /(jpe?g|png|gif|css|js)$/
-        
-      app.post '/login' do
+      m.set :login_success_message, "You have successfully logged in."
+      m.set :login_error_message,   "Wrong Email and/or Password combination."
+      m.set :login_url,             "/login"
+      m.set :login_user_class,      lambda { ::User }
+      m.set :ignored_by_return_to,  /(jpe?g|png|gif|css|js)$/
+
+      m.post "/login" do
         if authenticate(params)
           session[:success] = settings.login_success_message
           redirect_to_return_url
@@ -30,30 +30,28 @@ module Sinatra
         end
       end
     end
-    
+
     # Allows you to declaratively declare secured locations based on their
     # path prefix.
     #
     # @example
-    #   
-    #   require_login '/admin'
     #
-    #   get '/admin/posts' do
+    #   require_login "/admin"
+    #
+    #   get "/admin/posts" do
     #     # Posts here
     #   end
     #
-    #   get '/admin/users' do
+    #   get "/admin/users" do
     #     # Users here
     #   end
     #
-    # @param [#to_s] path_prefix a string to match against the start of 
+    # @param [#to_s] path_prefix a string to match against the start of
     #                request.fullpath
     # @return [nil]
     def require_login(path_prefix)
       before do
-        if request.fullpath =~ /^#{path_prefix}/
-          require_login
-        end
+        require_login if request.fullpath.start_with?(path_prefix)
       end
     end
   end
